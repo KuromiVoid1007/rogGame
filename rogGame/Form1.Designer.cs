f22a0b942bc7509399847b1557e819ac7edda922
KuromiVoid1007 <coolvip1007@mail.ru>
2024-04-10 03:30:41 +0500

    FormUi

--- rogGame/Form1.Designer.cs
+++ rogGame/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace rogGame
+﻿using System.Windows.Forms;
+
+namespace rogGame
 {
     partial class Form1
     {
@@ -28,13 +30,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.quickPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonExit = new System.Windows.Forms.PictureBox();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.quickPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // quickPanel
+            // 
+            this.quickPanel.BackColor = System.Drawing.Color.Transparent;
+            this.quickPanel.BackgroundImage = global::rogGame.Properties.Resources.panel;
+            this.quickPanel.Controls.Add(this.pictureBox1);
+            this.quickPanel.Controls.Add(this.pictureBox2);
+            this.quickPanel.Controls.Add(this.buttonExit);
+            this.quickPanel.Location = new System.Drawing.Point(0, 0);
+            this.quickPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.quickPanel.Name = "quickPanel";
+            this.quickPanel.Size = new System.Drawing.Size(1280, 25);
+            this.quickPanel.TabIndex = 0;
+            this.quickPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.quickPanel_MouseDown);
+            this.quickPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.quickPanel_MouseMove);
+            this.quickPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.quickPanel_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::rogGame.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::rogGame.Properties.Resources.buttonShow;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(1230, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(18, 18);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonExit.BackColor = System.Drawing.Color.Transparent;
+            this.buttonExit.BackgroundImage = global::rogGame.Properties.Resources.buttonExit;
+            this.buttonExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonExit.ErrorImage = null;
+            this.buttonExit.InitialImage = null;
+            this.buttonExit.Location = new System.Drawing.Point(1253, 3);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(18, 18);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.TabStop = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.Black;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(3, 720);
+            this.panelLeft.TabIndex = 3;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.Black;
+            this.panelRight.Location = new System.Drawing.Point(1277, 0);
+            this.panelRight.Margin = new System.Windows.Forms.Padding(0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(3, 720);
+            this.panelRight.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 717);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1280, 3);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.quickPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.quickPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonExit)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel quickPanel;
+        private System.Windows.Forms.PictureBox buttonExit;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Panel panelLeft;
+        private Panel panelRight;
+        private Panel panel1;
     }
 }
 
--- rogGame/Form1.cs
+++ rogGame/Form1.cs
@@ -12,9 +12,47 @@ namespace rogGame
 {
     public partial class Form1 : Form
     {
+        Form From2;
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+
         public Form1()
         {
             InitializeComponent();
+            Start();
         }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void Start()
+        {
+            From2 = new Form2();
+            From2.Show();
+        }
+
+        // настрйока панели
+        private void quickPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void quickPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void quickPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+        
     }
 }
